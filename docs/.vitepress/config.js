64b24967e8ed42ad48177baeafe3232245f94fb6
Jacob Lin <jacoblindev@gmail.com>
2022-11-08 22:08:45 +0800

    init: v2 of my blog site

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+    lang: 'zh-TW',
+    title: "JLDev",
+    titleTemplate: "JLDev",
+    description: "Jacob Lin's personal developer notes.",
+    appearance: "dark",
+    lastUpdated: true,
+
+    head: [
+        ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }]
+        // would render: <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    ],
+
+    themeConfig: {
+        logo: '/logo.svg',
+        nav: [
+            
+        ]
+    }
+
+}
